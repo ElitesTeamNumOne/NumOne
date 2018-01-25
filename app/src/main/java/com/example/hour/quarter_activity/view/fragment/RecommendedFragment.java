58c8123541ec538e28fff3b96d73593fb09109e3
wangguojian <1738609877@qq.com>
2018-01-25 09:33:36 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/java/com/example/hour/quarter_activity/view/fragment/VideoFragment.java
    #	app/src/main/res/values/colors.xml

--- app/src/main/java/com/example/hour/quarter_activity/view/fragment/RecommendedFragment.java
+++ app/src/main/java/com/example/hour/quarter_activity/view/fragment/RecommendedFragment.java
@@ -39,11 +39,11 @@ public class RecommendedFragment extends Fragment{
 
         //将TabLayout与ViewPager绑定在一起
         mTabLayout = (TabLayout) view.findViewById(R.id.tabLayout);
-        mTabLayout.setTabGravity(24);
         mTabLayout.setupWithViewPager(mViewPager);
 
         //指定Tab的位置
         one = mTabLayout.getTabAt(0);
         two = mTabLayout.getTabAt(1);
     }
+
 }
