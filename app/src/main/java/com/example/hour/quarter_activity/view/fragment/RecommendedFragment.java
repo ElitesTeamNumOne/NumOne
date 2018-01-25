b6607c81cb5756c070ae10d861769eae2b91254e
wangguojian <1738609877@qq.com>
2018-01-25 10:27:11 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/java/com/example/hour/quarter_activity/view/fragment/VideoFragment.java
    #	app/src/main/res/values/colors.xml

--- app/src/main/java/com/example/hour/quarter_activity/view/fragment/RecommendedFragment.java
+++ app/src/main/java/com/example/hour/quarter_activity/view/fragment/RecommendedFragment.java
@@ -1,16 +1,21 @@
 package com.example.hour.quarter_activity.view.fragment;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.example.hour.quarter_activity.R;
 import com.example.hour.quarter_activity.view.adapter.RecommendedAdapter;
+
+import java.lang.reflect.Field;
 
 /**
  * Created by 小社会 on 2018/1/23,0023.
@@ -31,7 +36,6 @@ public class RecommendedFragment extends Fragment{
         return view;
     }
     private void initViews() {
-
         //适配器将ViewPager与Fragment绑定在一起
         mViewPager = (ViewPager) view.findViewById(R.id.viewPager);
         recommendedAdapter = new RecommendedAdapter(getActivity().getSupportFragmentManager());
@@ -45,5 +49,45 @@ public class RecommendedFragment extends Fragment{
         one = mTabLayout.getTabAt(0);
         two = mTabLayout.getTabAt(1);
     }
+    public void setIndicator(TabLayout tabs, int leftDip, int rightDip) {
+        Class<?> tabLayout = tabs.getClass();
+        Field tabStrip = null;
+        try {
+            tabStrip = tabLayout.getDeclaredField("mTabStrip");
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        }
 
+        tabStrip.setAccessible(true);
+        LinearLayout llTab = null;
+        try {
+            llTab = (LinearLayout) tabStrip.get(tabs);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+        int left = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, leftDip, Resources.getSystem().getDisplayMetrics());
+        int right = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, rightDip, Resources.getSystem().getDisplayMetrics());
+
+        for (int i = 0; i < llTab.getChildCount(); i++) {
+            View child = llTab.getChildAt(i);
+            child.setPadding(0, 0, 0, 0);
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.MATCH_PARENT, 1);
+            params.leftMargin = left;
+            params.rightMargin = right;
+            child.setLayoutParams(params);
+            child.invalidate();
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mTabLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                setIndicator(mTabLayout, 100, 100);
+            }
+        });
+    }
 }
