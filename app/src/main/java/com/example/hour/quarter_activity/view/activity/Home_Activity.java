df9352b65e442cb366680445a3c1804da0c36a4a
wangguojian <1738609877@qq.com>
2018-01-26 18:43:00 +0800

    王国建

--- app/src/main/java/com/example/hour/quarter_activity/view/activity/Home_Activity.java
+++ app/src/main/java/com/example/hour/quarter_activity/view/activity/Home_Activity.java
@@ -71,7 +71,7 @@ public class Home_Activity extends FragmentActivity {
             }
         });
     }
-    //沉浸式大佬
+    //沉浸式
     @Override
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
