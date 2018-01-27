72a56a208158a52b485f4f9da785d4f7c62aafd9
wangguojian <1738609877@qq.com>
2018-01-27 10:42:54 +0800

    王国建

--- app/src/main/java/com/example/hour/quarter_activity/view/IView/VoidView/Viodview.java
+++ app/src/main/java/com/example/hour/quarter_activity/view/IView/VoidView/Viodview.java
@@ -1,12 +1,10 @@
 package com.example.hour.quarter_activity.view.IView.VoidView;
 
-import com.example.hour.quarter_activity.model.bean.HotBean;
-
 /**
  * Created by BoBrother on 2018/1/27.
  */
 
 public interface Viodview {
-    void succer(HotBean hotBean);
+
     void Finde(Exception e);
 }
