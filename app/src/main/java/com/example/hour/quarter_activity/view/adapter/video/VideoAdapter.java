44c4590510124a87b2d91e3141786b36e6ad4d14
BoBrother <409506980@qq.com>
2018-01-30 10:30:25 +0800

    张永博

---
+++ app/src/main/java/com/example/hour/quarter_activity/view/adapter/video/VideoAdapter.java
@@ -0,0 +1,74 @@
+package com.example.hour.quarter_activity.view.adapter.video;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.example.hour.quarter_activity.R;
+import com.example.hour.quarter_activity.model.bean.DataHot;
+import com.example.hour.quarter_activity.view.adapter.recommended.HotAdapterOne;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import cn.jzvd.JZVideoPlayerStandard;
+
+/**
+ * Created by BoBrother on 2018/1/27.
+ */
+
+public class VideoAdapter extends RecyclerView.Adapter {
+    private Context context;
+    private List<DataHot.DataBean> list_data;
+
+    public VideoAdapter(Context context, List<DataHot.DataBean> list) {
+        this.context = context;
+        this.list_data = list;
+
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = View.inflate(context, R.layout.vdio_hotlayout, null);
+        MyHolderOne holderOne = new MyHolderOne(view);
+        return holderOne;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        VideoAdapter.MyHolderOne holderOne = (VideoAdapter.MyHolderOne) holder;
+
+
+        holderOne.videoplayer.setUp(list_data.get(position).getVideoUrl()
+                , JZVideoPlayerStandard.SCREEN_WINDOW_NORMAL, "今日推荐");
+        Picasso.with(context)
+                .load(list_data.get(position).getCover())
+                .into(holderOne.videoplayer.thumbImageView);
+        holderOne.videoplayer.thumbImageView.setScaleType(ImageView.ScaleType.FIT_XY);
+//        TranslateAnimation animation = new TranslateAnimation()
+    }
+
+    @Override
+    public int getItemCount() {
+        if (list_data != null) {
+            return list_data.size();
+        }
+        return 0;
+    }
+
+    public class MyHolderOne extends RecyclerView.ViewHolder {
+        @BindView(R.id.videoplayer)
+        JZVideoPlayerStandard videoplayer;
+
+        public MyHolderOne(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+    }
+
+
+}
--- app/src/main/java/com/example/hour/quarter_activity/view/fragment/videofragment/HotFragment.java
+++ app/src/main/java/com/example/hour/quarter_activity/view/fragment/videofragment/HotFragment.java
@@ -17,6 +17,9 @@ import com.example.hour.quarter_activity.model.bean.AdvertisingHot;
 import com.example.hour.quarter_activity.model.bean.DataHot;
 import com.example.hour.quarter_activity.presenter.Recommended.HotPresenter;
 import com.example.hour.quarter_activity.view.IView.Recommended.IHotView;
+import com.example.hour.quarter_activity.view.IView.VoidView.SpacesItemDecoration;
+import com.example.hour.quarter_activity.view.adapter.recommended.HotAdapterOne;
+import com.example.hour.quarter_activity.view.adapter.video.VideoAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,15 +32,16 @@ public class HotFragment extends Fragment implements IHotView {
     private List<DataHot.DataBean> list=new ArrayList<>();
     private RecyclerView recyclerview;
     HotPresenter presenter;
+    VideoAdapter adapterOne;
     View view;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = View.inflate(getActivity(), R.layout.hot_fragment, null);
+        recyclerview = view.findViewById(R.id.hot_rv);
         presenter = new HotPresenter(this);
+
         presenter.getDataTwo();
-
-
         return view;
     }
 
@@ -51,8 +55,13 @@ public class HotFragment extends Fragment implements IHotView {
     public void onSucceedTwo(DataHot dataHot) {
         List<DataHot.DataBean> data = dataHot.getData();
         //设置布局管理器
-        final StaggeredGridLayoutManager manager = new StaggeredGridLayoutManager(3,StaggeredGridLayoutManager.VERTICAL);
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
+        final StaggeredGridLayoutManager manager = new StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL);
         recyclerview.setLayoutManager(manager);
+        adapterOne = new VideoAdapter(getActivity(),data);
+        recyclerview.setAdapter(adapterOne);
+        //设置item之间的间隔
+        SpacesItemDecoration decoration=new SpacesItemDecoration(16);
+        recyclerview.addItemDecoration(decoration);
+
     }
 }
