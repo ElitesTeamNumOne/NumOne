0a0cabd68ae6e81ea8970177001888fca5d40e94
wangguojian <1738609877@qq.com>
2018-02-03 14:56:12 +0800

    王国建

--- app/src/main/java/com/example/hour/quarter_activity/utils/PortApi.java
+++ app/src/main/java/com/example/hour/quarter_activity/utils/PortApi.java
@@ -13,20 +13,16 @@ import rx.Observable;
 /**
  * Created by wangguojian on 2018/1/25.
  */
-
 public interface PortApi {
     @GET("quarter/getAd")
     Observable<AdvertisingHot> getDataOne();
     //https://www.zhaoapi.cn/quarter/getVideos?uid=11864&type=1&page=1&source=android&appVersion=1
     @GET("quarter/getVideos")
     Observable<DataHot>getDataTwo(@Query("type") String type,@Query("page") String page);
-
-
     //段子列表
     //https://www.zhaoapi.cn/quarter/getJokes?page=10&source=android&appVersion=101
     @GET("quarter/getJokes")
     Observable<Jokes_Bean> getdata(@Query("page") String page);
-
     //段子点赞
     //https://www.zhaoapi.cn/quarter/jokePraise?uid=11864&jid=1312&token=2A27DEA57FF5BAADE1A75CC224108424&source=android&appVersion=1
     @GET("quarter/jokePraise")
